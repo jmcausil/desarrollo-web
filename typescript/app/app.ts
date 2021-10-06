1f7edf08ef30bc3276cd86c141f985a7742d3c8b
jose causil <jmcausil@uniguajira.edu.co>
2021-10-06 18:22:33 -0500

    .

--- typescript/app/app.ts
+++ typescript/app/app.ts
@@ -3,9 +3,48 @@
 
 
 //string
-let nombre:string = "jose causil";
+let nombre:string = "jose causil guerra";
 console.log(`el cliente ${nombre}`);
 
 //numeros
 let numero: number = 75;
 console.log(`el valor es ${numero}`);
+
+//array
+
+let lista:string[] = ["bogota","guajira","pasto"];
+console.log(lista);
+
+let lista1:number[] = [1,2,3];
+console.log(lista1);
+
+// let lista2:string[] = ["bogota","guajira","cucuta"];
+// for (let index = 0; index < lista2.length; index++){
+    // const element = lista2[index];
+    // console.log(element);
+
+// }
+
+//funcion mas sofisticada que la anterior
+
+lista.forEach(element => {
+    console.log(element)
+});
+
+//tuplas
+
+// let tuple 
+
+
+
+
+
+
+
+
+
+
+
+
+
+
--- typescript/js/app.js
+++ typescript/js/app.js
@@ -1,5 +1,12 @@
 "use strict";
-let nombre = "jose causil";
+let nombre = "jose causil guerra";
 console.log(`el cliente ${nombre}`);
 let numero = 75;
 console.log(`el valor es ${numero}`);
+let lista = ["bogota", "guajira", "pasto"];
+console.log(lista);
+let lista1 = [1, 2, 3];
+console.log(lista1);
+lista.forEach(element => {
+    console.log(element);
+});
